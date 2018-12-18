ee8deb796fe0a3172699431774b178274fd69def
邓锋 <704292743@qq.com>
2018-12-18 10:09:52 +0800

    主线程

--- ScreenRecorder/Classes/Core/SRViewRecorder.swift
+++ ScreenRecorder/Classes/Core/SRViewRecorder.swift
@@ -88,7 +88,9 @@ public extension SRViewRecorder{
             if succ{
                 let op = BlockOperation.init {[weak self] in
                     guard let `self` = self,!self.isRecording,self.maxDuration > self.duration else{return}
-                    UIApplication.shared.isIdleTimerDisabled = true
+                    DispatchQueue.main.async {
+                        UIApplication.shared.isIdleTimerDisabled = true
+                    }
                     self.isRecording = true
                     self.viewCapture.start()
                     self.micCapture.start()
@@ -114,7 +116,9 @@ public extension SRViewRecorder{
     public func pause(){
         let op = BlockOperation.init {[weak self] in
             guard let `self` = self,self.isRecording else{return}
-            UIApplication.shared.isIdleTimerDisabled = false
+            DispatchQueue.main.async {
+                UIApplication.shared.isIdleTimerDisabled = false
+            }
             self.isRecording = false
             self.writer.stop()
             self.viewCapture.pause()
